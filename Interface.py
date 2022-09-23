151dd1b53fe0306909719b77821d9fcc5a05e8a8
YSpiegel <spiegel.yoav@gmail.com>
2022-09-23 15:43:42 +0300

    setup for working on interface

--- Interface.py
+++ Interface.py
@@ -1,5 +1,8 @@
-import Database as db
+# import Database as Db
 import tkinter as tk
 
+window = tk.Tk()
 
-print()
+general_BDays = {"29/09": ["Ani Ido Kedem"],"26/07": ["Yoav Spiegel"], "23/09": ["nobody", "no one"]}
+
+window.mainloop()
