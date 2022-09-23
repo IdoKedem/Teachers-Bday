910e28dda7e376d7dcf42d8d293f9e40879bcb2a
YSpiegel <spiegel.yoav@gmail.com>
2022-09-23 23:11:39 +0300

    almost done, required massive rearrangement and modification. works tho

--- Interface.py
+++ Interface.py
@@ -7,6 +7,7 @@ window = tk.Tk()
 window.geometry("600x400")
 window.title("Teachers Birthdays by Yoav Spiegel and Ido Kedem")
 
+
 def format_date(input_date):
     formatted = input_date.strftime("%x")
     return formatted[3:6] + formatted[0:2]
@@ -18,12 +19,11 @@ random_date = dt.date(year=2022, month=9, day=22)
 global current_date
 current_date = random_date.today()
 date_label = tk.Label(window, text=format_date(current_date), font=("arial", 20))
-date_label.place(x=270, y=300)
+date_label.place(x=255, y=300)
 # tomorrow = current_date + dt.timedelta(days=1)
 today = format_date(current_date)
 who_has_bd = tk.Label(window, text="Teachers who have a birthday today: ", font=("arial", 20), pady=10)
 who_has_bd.pack()
-
 
 general_BDays = {"29/09": ["Ani Ido Kedem"], "26/07": ["Yoav Spiegel"], "25/09": ["nobody", "no one"]}  # until real one
 
@@ -34,15 +34,18 @@ for date in general_BDays:
         for teacher in general_BDays[date]:
             teachers_label_text += teacher + "\n"
 
-
 teachers_label = tk.Label(window, text=teachers_label_text, font=("arial", 12))
 teachers_label.pack()
 
-# Second part: functions for buttons
+
+# Second part: date buttons functions
 
 def update_to_date():
     global current_date
     teachers_label_text = ""
+
+    formatted_date = format_date(current_date)
+    date_label.configure(text=formatted_date)
 
     if current_date == random_date.today():  # return to basic format
         who_has_bd.configure(text="Teachers who have a birthday today: ")
@@ -53,7 +56,6 @@ def update_to_date():
                     teachers_label_text += teacher + "\n"
 
     else:  # update the title and list according to the changed date
-        formatted_date = format_date(current_date)
         who_has_bd.configure(text="Teachers who have a birthday on " + formatted_date)
 
         for date in general_BDays:
@@ -62,6 +64,15 @@ def update_to_date():
                     teachers_label_text += teacher + "\n"
 
     teachers_label.configure(text=teachers_label_text)
+
+
+# mode change button must be here
+def mode_change():
+    update_to_date()
+    return_to_date_mode.place_forget()
+
+
+return_to_date_mode = tk.Button(window, text="Return to date mode", command=mode_change)
 
 
 def update_to_month(month, month_name):
@@ -73,6 +84,8 @@ def update_to_month(month, month_name):
             for teacher in general_BDays[date]:
                 teachers_label_text += teacher + "\n"
     teachers_label.configure(text=teachers_label_text)
+    return_to_date_mode.place(x=230, y=350)
+
 
 
 def minus_day():
@@ -87,12 +100,40 @@ def plus_day():
     update_to_date()
 
 
-# Third part: buttons and date interface
+# Third part: date buttons
 
-minus_day = tk.Button(window, text="⮝", width=4, height=2, command=minus_day)
-minus_day.place(x=225, y=300)
+plus_day = tk.Button(window, text="⮝", width=4, height=2, command=plus_day)
+plus_day.place(x=210, y=300)
 
-plus_day = tk.Button(window, text="⮟", width=4, height=2, command=plus_day)
-plus_day.place(x=350, y=300)
+minus_day = tk.Button(window, text="⮟", width=4, height=2, command=minus_day)
+minus_day.place(x=335, y=300)
+
+
+jan = tk.Button(window, text="January", command=lambda: update_to_month("01", "January"), width=10)
+jan.place(x=25, y=75)
+feb = tk.Button(window, text="February", command=lambda: update_to_month("02", "February"), width=10)
+feb.place(x=25, y=125)
+mar = tk.Button(window, text="March", command=lambda: update_to_month("03", "March"), width=10)
+mar.place(x=25, y=175)
+apr = tk.Button(window, text="April", command=lambda: update_to_month("04", "April"), width=10)
+apr.place(x=25, y=225)
+may = tk.Button(window, text="May", command=lambda: update_to_month("05", "May"), width=10)
+may.place(x=25, y=275)
+jun = tk.Button(window, text="June", command=lambda: update_to_month("06", "June"), width=10)
+jun.place(x=25, y=325)
+
+
+jul = tk.Button(window, text="July", command=lambda: update_to_month("07", "July"), width=10)
+jul.place(x=495, y=75)
+aug = tk.Button(window, text="August", command=lambda: update_to_month("08", "August"), width=10)
+aug.place(x=495, y=125)
+sep = tk.Button(window, text="September", command=lambda: update_to_month("09", "September"), width=10)
+sep.place(x=495, y=175)
+oct = tk.Button(window, text="October", command=lambda: update_to_month("10", "October"), width=10)
+oct.place(x=495, y=225)
+nov = tk.Button(window, text="November", command=lambda: update_to_month("11", "November"), width=10)
+nov.place(x=495, y=275)
+dec = tk.Button(window, text="December", command=lambda: update_to_month("12", "December"), width=10)
+dec.place(x=495, y=325)
 
 window.mainloop()
