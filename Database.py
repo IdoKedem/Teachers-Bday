fd3f6fd7cf242af28b273e712d40fda0966fb2dc
IdoKedem <idokedem12@gmail.com>
2022-12-24 21:44:46 +0200

    added is_duplicate func

--- Database.py
+++ Database.py
@@ -12,7 +12,6 @@ def fix_date(date: str) -> str:
     lst = [("0" + i if len(i) == 1 else i) for i in lst]  # adds "0" to day or month if needed
     return "/".join(lst)
 
-
 def load_database_dict():
     """
     Reads xlsx file and loads python dict
@@ -43,10 +42,29 @@ def add_teacher_to_db(date: str, full_name: str) -> None:
     wb.save('Teachers-Bday.xlsx')
 
 def search_teachers(search_input: str) -> dict[str: list[str]]:
+    """
+    searches a teacher according to a given string
+    :param search_input: name or part of teacher
+    :return: a dict featuring all teachers after search
+    """
     teachers_dict = load_database_dict()
     search_result_dict = defaultdict(lambda: [])
+
     for date, teacher_list in teachers_dict.items():
         for teacher in teacher_list:
             if search_input in teacher:
                 search_result_dict[date].append(teacher)
     return search_result_dict
+
+def is_duplicate_teacher(name: str) -> bool:
+    """
+    checks if a teacher already exists in database
+    :param name: teacher's name
+    :return: True if already exists, False otherwise
+    """
+    teachers_dict = load_database_dict()
+    for date, teacher_list in teachers_dict.items():
+        for teacher in teacher_list:
+            if name == teacher:
+                return True
+    return False
